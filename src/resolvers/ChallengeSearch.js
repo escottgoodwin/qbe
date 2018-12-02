71703a76a09a7bce0c844dc48551d57ce3a56a85
Evan Goodwin <escottgoodwin@gmail.com>
2018-12-02 15:58:46 -0800

    fixed sort

--- src/resolvers/ChallengeSearch.js
+++ src/resolvers/ChallengeSearch.js
@@ -1,6 +1,6 @@
 function challenges(parent, args, ctx, info) {
   const args1 = parent.args1
-  return ctx.db.query.challenges({ where: { id_in: parent.challengeIds }, { skip, first, orderBy } = parent.args1 }, info)
+  return ctx.db.query.challenges({ where: { id_in: parent.challengeIds }, skip, first, orderBy } = parent.args1 }, info)
 }
 
 module.exports = {
