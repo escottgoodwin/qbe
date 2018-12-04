00e9ee0ca755dcda4c7c527f355fd0788b1e1da0
Evan Goodwin <escottgoodwin@gmail.com>
2018-12-03 21:16:17 -0800

    completed signup, login, reset pswd, confirm email

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const crypto = require('crypto')
 const { APP_SECRET, getUserId, getUser } = require('../utils')
 const util = require('util');
 
@@ -13,31 +14,274 @@ function checkField(itemIds) {
   }
 }
 
+function sendGridSend(msg){
+  const sgMail = require('@sendgrid/mail');
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  const msgSg = {
+    to: msg.to,
+    from: 'quandria_help@quandria.com',
+    subject:msg.subject,
+    text: msg.text,
+    html:msg.html,
+    };
+
+    sgMail.send(msgSg);
+  }
+
+
 async function signup(parent, args, ctx, info) {
 
   const signUpDate = new Date()
 
   const password = await bcrypt.hash(args.password, 10)
-  // 2
-  const user = await ctx.db.mutation.createUser({
-    data: { ...args, password, signUpDate },
-  }, `{ id }`)
 
-  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  const confirmationToken = await crypto.randomBytes(20).toString('hex')
+  const tokenExpirationTime = signUpDate
+  tokenExpirationTime.setHours(signUpDate.getHours() + 2);
+
+  const user = await ctx.db.mutation.createUser({
+    data: { ...args,
+          password,
+          signUpDate,
+          confirmationToken,
+          tokenExpirationTime,
+          },
+  }, `{ id firstName lastName email role confirmed }`)
+
+  const htmlEmail =
+    `<html>
+    <head>
+      <title>Confirm your email address</title>
+    </head>
+    <body>
+      <p>Hi ${ user.firstName} ${ user.lastName },</p>
+      <p><a href="https://example.com/reset?token=${confirmationToken}&email=${user.email}">Click here to confirm your email address.</a></p>
+      <p>This token will expire in 2 hours.</p>
+    </body>
+    </html>`
+
+  const msg = {
+    to: args.email,
+    subject: 'Confirm your email address',
+    text: `Click on this link to resent your password https://example.com/reset?token=${confirmationToken}&email=${user.email} This token will expire in 2 hours.`,
+    html: htmlEmail,
+  };
+
+  sendGridSend(msg)
+
+
+  signUpRequestMsg = user.firstName + ' ' + user.lastName + ', thank you for signing up. We will send you an email confirmation. Once you confirm your email address, you will be able to login.'
 
   return {
-    token,
-    user,
+    authMsg: signUpRequestMsg,
+    user
   }
+
+}
+
+async function newPasswordRequest(parent, args, ctx, info) {
+
+  const resetUser = await ctx.db.query.user( { where: { email: args.email } } ,
+  `{ id }`)
+  const ruser = JSON.stringify(resetUser)
+
+  if (!resetUser) {
+    throw new Error(`No such user found ${ruser}`)
+  }
+
+  const resetToken = await crypto.randomBytes(20).toString('hex')
+  const now = new Date()
+  const tokenExpirationTime = now
+  tokenExpirationTime.setHours(tokenExpirationTime.getHours() + 2);
+  // 2
+  const userResetUpdate = await ctx.db.mutation.updateUser(
+    {
+      data: {
+        resetToken,
+        tokenExpirationTime,
+      },
+      where: {
+        id: resetUser.id
+      },
+    },
+    `{ id firstName lastName email }`
+  )
+
+  const htmlEmail =
+    `<html>
+    <head>
+      <title>Reset your password</title>
+    </head>
+    <body>
+      <p>Hi ${userResetUpdate.firstName} ${userResetUpdate.lastName},</p>
+      <p><a href="https://example.com/reset?token=${resetToken}&email=${userResetUpdate.email}">Click here to reset your password.</a></p>
+      <p>This token will expire in 2 hours.</p>
+    </body>
+    </html>`
+
+  const msg = {
+    to: userResetUpdate.email,
+    subject: 'Reset password request',
+    text: `Click on this link to resent your password https://example.com/reset?token=${resetToken}&email=${userResetUpdate.email} This token will expire in 2 hours.`,
+    html: htmlEmail,
+  };
+
+  sendGridSend(msg)
+
+  resetRequestMsg = userResetUpdate.firstName + ' ' + userResetUpdate.lastName + ', you have requested to reset your password. Please check for a reset email. Click the link in the email to reset your password.'
+
+  return {
+    authMsg: resetRequestMsg,
+    user: userResetUpdate
+  }
+
+}
+
+async function resetPassword(parent, args, ctx, info) {
+
+  const now = new Date()
+
+  const resetUser  = await ctx.db.query.users(
+    {
+      where: {
+        email: args.email,
+        resetToken: args.resetToken,
+        tokenExpirationTime_gte: now
+    }
+  },
+    `{ id email }`
+  )
+
+  if (resetUser.length<1) {
+    throw new Error(`Email confirmation error. Please try again.`)
+  }
+
+  const password = await bcrypt.hash(args.resetPassword, 10)
+
+  const userResetUpdate = await ctx.db.mutation.updateUser(
+    {
+      data: {
+        password,
+        resetToken: null,
+        tokenExpirationTime: null,
+      },
+      where: {
+        id: resetUser[0].id
+      },
+    },
+    `{ id firstName lastName email }`
+  )
+
+  const htmlEmail =
+    `<html>
+    <head>
+      <title>Your password has been reset</title>
+    </head>
+    <body>
+      <p>Hi ${userResetUpdate.firstName} ${userResetUpdate.lastName},</p>
+      <p>Your password has been reset.</a></p>
+      <p>If this is an error, please login.</p>
+    </body>
+    </html>`
+
+  const msg = {
+    to: args.email,
+    subject: 'Password reset',
+    text: 'Your password has been reset. If this is an error, please report it.',
+    html: htmlEmail,
+  };
+
+  sendGridSend(msg)
+
+  resetRequestMsg = userResetUpdate.firstName + ' ' + userResetUpdate.lastName + ', you have reset your password. We will send you an email confirmation.'
+
+  return {
+    authMsg: resetRequestMsg,
+    user: userResetUpdate
+  }
+
+}
+
+async function confirmEmail(parent, args, ctx, info) {
+
+  const now = new Date()
+
+  const confirmUser1 = await ctx.db.query.users(
+    {
+      where: {
+        email: args.email,
+        confirmationToken: args.confirmationToken,
+        tokenExpirationTime_gte: now
+    }
+  },
+    `{ id }`
+  )
+
+  if (confirmUser1.length<1) {
+    throw new Error(`Email confirmation error. Please try again.`)
+  }
+
+  const confirmUser = await ctx.db.mutation.updateUser(
+    {
+      data: {
+        confirmed:true,
+        confirmationToken: null,
+        tokenExpirationTime: null,
+      },
+      where: {
+        id: confirmUser1[0].id
+      },
+    },
+    `{ id firstName lastName email role confirmed }`
+  )
+
+  const htmlEmail =
+    `<html>
+    <head>
+      <title>Your account email has been confirmed</title>
+    </head>
+    <body>
+      <p>Hi ${confirmUser.firstName} ${confirmUser.lastName},</p>
+      <p>Please login: <a href="https://example.com/login">Login</a></p>
+    </body>
+    </html>`
+
+  const msg = {
+    to: args.email,
+    subject: 'Account confirmed',
+    text: 'Your account has been confirmed. Please login in at https://example.com/login',
+    html: htmlEmail,
+  };
+
+  sendGridSend(msg)
+
+  confirmRequestMsg = confirmUser.firstName + ' ' + confirmUser.lastName + ', your account has been confirmed. Please login <a href="https://example.com/login">Login</a>.'
+
+  return {
+    authMsg: confirmRequestMsg,
+    user: confirmUser
+  }
+
 }
 
 async function login(parent, args, ctx, info) {
   const lastLogin = new Date()
 
-  const user = await ctx.db.query.user({ where: { email: args.email } }, ` { id password firstName lastName role } ` )
+  const user = await ctx.db.query.user(
+    {
+      where: {
+        email: args.email,
+      }
+    }, ` { id password firstName lastName role confirmed } ` )
   //` { id password firstName lastName role } `
   if (!user) {
-    throw new Error('No such user found')
+    throw new Error('No such user found.')
+  }
+
+  if (!user.confirmed) {
+    throw new Error('You have not confirmed your email account yet.')
   }
 
   const valid = await bcrypt.compare(args.password, user.password)
@@ -52,7 +296,7 @@ async function login(parent, args, ctx, info) {
         online:true
       },
       where: {
-        id: user.id
+        id: user.id,
       },
     },
     ` { id password firstName lastName role } `
@@ -825,7 +1069,7 @@ adminDepartmentIds,
 studentIds,
 teacherIds,
 phone,
-online
+online,
 }, ctx, info) {
 
   const userId = await getUserId(ctx)
@@ -871,6 +1115,9 @@ online
 
 module.exports = {
   signup,
+  newPasswordRequest,
+  resetPassword,
+  confirmEmail,
   login,
   addInstitution,
   updateInstitution,
