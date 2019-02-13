6da49def574b8949d3e0c51591b103606fdd4068
Evan Goodwin <escottgoodwin@gmail.com>
2019-02-12 16:02:33 -0800

    userquestion and user answer queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -48,35 +48,25 @@ async function institutions(parent, args, ctx, info) {
 
   const where = args.where
 
-  const where1 = args.filter
-      ? {
-          OR: [
-            { id: args.filter },
-            { name_contains: args.filter },
-            { type: args.filter },
-          ],
-        }
-      : {}
+  const queriedInstitutions = await ctx.db.query.institutions(
+  { where },
+  `{ id }`,
+)
 
-      const queriedInstitutions = await ctx.db.query.institutions(
-      { where },
-      `{ id }`,
-    )
-
-      const countSelectionSet = `
-        {
-          aggregate {
-            count
-          }
-        }
-      `
-      const institutionsConnection = await ctx.db.query.institutionsConnection( {where}, countSelectionSet)
-
-      return {
-        count: institutionsConnection.aggregate.count,
-        institutionIds: queriedInstitutions.map(institution => institution.id),
-        args1: args
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
       }
+    }
+  `
+  const institutionsConnection = await ctx.db.query.institutionsConnection( {where}, countSelectionSet)
+
+  return {
+    count: institutionsConnection.aggregate.count,
+    institutionIds: queriedInstitutions.map(institution => institution.id),
+    args1: args
+  }
 
 }
 
@@ -89,16 +79,6 @@ async function institution(parent, args, ctx, info) {
 async function courses(parent, args, ctx, info) {
 
     const where = args.where
-
-    const where1 = args.filter
-        ? {
-            OR: [
-              { id: args.filter },
-              { name_contains: args.filter },
-              { courseNumber_contains: args.filter }
-            ],
-          }
-        : {}
 
     const queriedCourses = await ctx.db.query.courses(
     { where },
@@ -159,7 +139,6 @@ async function userAnsweredStats(parent, args, ctx, info){
         }
       }
     `
-
     const answersConnection = await ctx.db.query.answersConnection({ where: { addedBy: { id: userId }, answer: { question: { test: { id: args.testId } } } } },
       countSelectionSet)
 
@@ -303,41 +282,30 @@ async function tests(parent, args, ctx, info) {
 
   const where = args.where
 
-  const where1 = args.filter
-      ? {
-          OR: [
-            { id: args.filter },
-            { subject_contains: args.filter },
-            { testNumber_contains: args.filter },
-          ],
-        }
-      : {}
+  const queriedTests = await ctx.db.query.tests(
+  { where },
+  `{ id }`,
+)
 
-
-      const queriedTests = await ctx.db.query.tests(
-      { where },
-      `{ id }`,
-    )
-
-      const countSelectionSet = `
-        {
-          aggregate {
-            count
-          }
-        }
-      `
-      const testsConnection = await ctx.db.query.testsConnection({ where }, countSelectionSet)
-
-      return {
-        count: testsConnection.aggregate.count,
-        testIds: queriedTests.map(test => test.id),
-        args1: args
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
       }
+    }
+  `
+  const testsConnection = await ctx.db.query.testsConnection({ where }, countSelectionSet)
+
+  return {
+    count: testsConnection.aggregate.count,
+    testIds: queriedTests.map(test => test.id),
+    args1: args
+  }
 }
 
 async function test(parent, args, ctx, info) {
 
-      return await ctx.db.query.test( { where: { id: args.id } },info,)
+  return await ctx.db.query.test( { where: { id: args.id } },info,)
 
 }
 
@@ -345,34 +313,25 @@ async function panels(parent, args, ctx, info) {
 
   const where = args.where
 
-  const where1 = args.filter
-      ? {
-          OR: [
-            { id: args.filter },
-            { link: args.filter },
-          ],
-        }
-      : {}
+  const queriedPanels = await ctx.db.query.panels(
+  { where },
+  `{ id }`,
+)
 
-      const queriedPanels = await ctx.db.query.panels(
-      { where },
-      `{ id }`,
-    )
-
-      const countSelectionSet = `
-        {
-          aggregate {
-            count
-          }
-        }
-      `
-      const panelsConnection = await ctx.db.query.panelsConnection({ where }, countSelectionSet)
-
-      return {
-        count: panelsConnection.aggregate.count,
-        panelIds: queriedPanels.map(panel => panel.id),
-        args1: args
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
       }
+    }
+  `
+  const panelsConnection = await ctx.db.query.panelsConnection({ where }, countSelectionSet)
+
+  return {
+    count: panelsConnection.aggregate.count,
+    panelIds: queriedPanels.map(panel => panel.id),
+    args1: args
+  }
 }
 
 async function questionStats(parent, args, ctx, info) {
@@ -400,70 +359,91 @@ async function responseImages(parent, args, ctx, info) {
 
   const where = args.where
 
-  const where1 = args.filter
-      ? {
-          OR: [
-            { id: args.filter },
-            { question_contains: args.filter },
-          ],
-        }
-      : {}
+  const queriedResponseImages = await ctx.db.query.responseImages(
+  { where },
+  `{ id }`,
+)
 
-      const queriedResponseImages = await ctx.db.query.responseImages(
-      { where },
-      `{ id }`,
-    )
-
-      const countSelectionSet = `
-        {
-          aggregate {
-            count
-          }
-        }
-      `
-
-      const responseImages = await ctx.db.query.responseImagesConnection({ where }, countSelectionSet)
-
-      return {
-        count: responseImages.aggregate.count,
-        responseImageIds: queriedResponseImages.map(question => question.id),
-        args1: args
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
       }
+    }
+  `
+
+  const responseImages = await ctx.db.query.responseImagesConnection({ where }, countSelectionSet)
+
+  return {
+    count: responseImages.aggregate.count,
+    responseImageIds: queriedResponseImages.map(question => question.id),
+    args1: args
+  }
 }
+
+
 
 async function questions(parent, args, ctx, info) {
 
   const where = args.where
 
-  const where1 = args.filter
-      ? {
-          OR: [
-            { id: args.filter },
-            { question_contains: args.filter },
-          ],
-        }
-      : {}
+  const queriedQuestions = await ctx.db.query.questions(
+  { where },
+  `{ id }`,
+)
 
-      const queriedQuestions = await ctx.db.query.questions(
-      { where },
-      `{ id }`,
-    )
-
-      const countSelectionSet = `
-        {
-          aggregate {
-            count
-          }
-        }
-      `
-
-      const questionsConnection = await ctx.db.query.questionsConnection({ where }, countSelectionSet)
-
-      return {
-        count: questionsConnection.aggregate.count,
-        questionIds: queriedQuestions.map(question => question.id),
-        args1: args
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
       }
+    }
+  `
+
+  const questionsConnection = await ctx.db.query.questionsConnection({ where }, countSelectionSet)
+
+  return {
+    count: questionsConnection.aggregate.count,
+    questionIds: queriedQuestions.map(question => question.id),
+    args1: args
+  }
+}
+
+async function userQuestions(parent, args, ctx, info) {
+
+    const userId = await getUserId(ctx)
+
+    return await ctx.db.query.questions(
+      {  where:
+        {
+          AND:[{
+          test:{ id: args.testId }
+        },
+        {
+        addedBy: { id: userId }
+      }]
+    }
+  },
+  info
+  )
+}
+
+async function userAnswers(parent, args, ctx, info) {
+
+      const userId = await getUserId(ctx)
+
+      return await ctx.db.query.answers(
+      {  where:
+        { AND: [{
+            question:{
+              test: { id: args.testId }
+            }
+            },
+            { addedBy: { id: userId } }
+          ]}
+      },
+      info
+  )
 }
 
 async function question(parent, args, ctx, info) {
@@ -523,68 +503,50 @@ async function questionchoices(parent, args, ctx, info) {
 
   const where = args.where
 
-  const where1 = args.filter
-      ? {
-          OR: [
-            { id: args.filter },
-            { choice_contains: args.filter },
-          ],
-        }
-      : {}
+  const queriedQuestionChoices = await ctx.db.query.questionChoices(
+  { where },
+  `{ id }`,
+)
 
-      const queriedQuestionChoices = await ctx.db.query.questionChoices(
-      { where },
-      `{ id }`,
-    )
-
-      const countSelectionSet = `
-        {
-          aggregate {
-            count
-          }
-        }
-      `
-      const questionChoicesConnection = await ctx.db.query.questionChoicesConnection({ where }, countSelectionSet)
-
-      return {
-        count: questionChoicesConnection.aggregate.count,
-        questionChoiceIds: queriedQuestionChoices.map(questionChoice => questionChoice.id),
-        args1: args
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
       }
+    }
+  `
+  const questionChoicesConnection = await ctx.db.query.questionChoicesConnection({ where }, countSelectionSet)
+
+  return {
+    count: questionChoicesConnection.aggregate.count,
+    questionChoiceIds: queriedQuestionChoices.map(questionChoice => questionChoice.id),
+    args1: args
+  }
 }
 
 async function challenges(parent, args, ctx, info) {
 
   const where = args.where
 
-  const where1 = args.filter
-      ? {
-          OR: [
-            { id: args.filter },
-            { challenge_contains: args.filter },
-          ],
-        }
-      : {}
+  const queriedChallenges = await ctx.db.query.challenges(
+  { where },
+  `{ id }`,
+)
 
-      const queriedChallenges = await ctx.db.query.challenges(
-      { where },
-      `{ id }`,
-    )
-
-      const countSelectionSet = `
-        {
-          aggregate {
-            count
-          }
-        }
-      `
-      const challengesConnection = await ctx.db.query.challengesConnection({ where }, countSelectionSet)
-
-      return {
-        count: challengesConnection.aggregate.count,
-        challengeIds: queriedChallenges.map(challenge => challenge.id),
-        args1: args
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
       }
+    }
+  `
+  const challengesConnection = await ctx.db.query.challengesConnection({ where }, countSelectionSet)
+
+  return {
+    count: challengesConnection.aggregate.count,
+    challengeIds: queriedChallenges.map(challenge => challenge.id),
+    args1: args
+  }
 }
 
 async function challenge(parent, args, ctx, info) {
@@ -597,67 +559,50 @@ async function challengeMessages(parent, args, ctx, info) {
 
   const where = args.where
 
-  const where1 = args.filter
-      ? {
-          OR: [
-            { id: args.filter },
-            { challengeMessage_contains: args.filter },
-          ],
-        }
-      : {}
+  const queriedChallengeMessages = await ctx.db.query.challengeMessages(
+  { where },
+  `{ id }`,
+)
 
-      const queriedChallengeMessages = await ctx.db.query.challengeMessages(
-      { where },
-      `{ id }`,
-    )
-
-      const countSelectionSet = `
-        {
-          aggregate {
-            count
-          }
-        }
-      `
-      const challengeMessagesConnection = await ctx.db.query.challengeMessagesConnection({ where }, countSelectionSet)
-
-      return {
-        count: challengeMessagesConnection.aggregate.count,
-        challengeMessageIds: queriedChallengeMessages.map(challengeMessage => challengeMessage.id),
-        args1: args
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
       }
+    }
+  `
+  const challengeMessagesConnection = await ctx.db.query.challengeMessagesConnection({ where }, countSelectionSet)
+
+  return {
+    count: challengeMessagesConnection.aggregate.count,
+    challengeMessageIds: queriedChallengeMessages.map(challengeMessage => challengeMessage.id),
+    args1: args
+  }
 }
 
 async function answers(parent, args, ctx, info) {
 
   const where = args.where
 
-  const where1 = args.filter
-      ? {
-          OR: [
-            { id: args.filter },
-          ],
-        }
-      : {}
+  const queriedAnswers = await ctx.db.query.answers(
+  { where },
+  `{ id }`,
+)
 
-      const queriedAnswers = await ctx.db.query.answers(
-      { where },
-      `{ id }`,
-    )
-
-      const countSelectionSet = `
-        {
-          aggregate {
-            count
-          }
-        }
-      `
-      const answersConnection = await ctx.db.query.answersConnection({ where }, countSelectionSet)
-
-      return {
-        count: answersConnection.aggregate.count,
-        answerIds: queriedAnswers.map(answer => answer.id),
-        args1: args
+  const countSelectionSet = `
+    {
+      aggregate {
+        count
       }
+    }
+  `
+  const answersConnection = await ctx.db.query.answersConnection({ where }, countSelectionSet)
+
+  return {
+    count: answersConnection.aggregate.count,
+    answerIds: queriedAnswers.map(answer => answer.id),
+    args1: args
+  }
 }
 
 async function answer(parent, args, ctx, info) {
@@ -670,15 +615,7 @@ async function sequences(parent, args, ctx, info) {
 
   const where = args.where
 
-  const where1 = args.filter
-      ? {
-          OR: [
-            { id: args.filter },
-          ],
-        }
-      : {}
-
-    return await ctx.db.query.sequences({ where }, info)
+  return await ctx.db.query.sequences({ where }, info)
 }
 
 
@@ -701,6 +638,8 @@ module.exports = {
   responseImages,
   questions,
   question,
+  userQuestions,
+  userAnswers,
   questionsAnswered,
   questionStats,
   questionchoices,
